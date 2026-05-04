392e4442023a8c35ec48200a252fadf322b96dfc
Wae Outte.J <j.mckenz@live.com>
2026-05-04 15:14:15 -0400

    Changes to be committed: 	new file:   app/show_columns.py 	new file:   queries/by_Zone.sql 	new file:   queries/identify_Headers.sql

---
+++ app/show_columns.py
@@ -0,0 +1,13 @@
+import sqlite3
+import os
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+database_path = os.path.join(base_dir, 'atlanta_crime.db')
+
+conn = sqlite3.connect(database_path)
+cursor = conn.cursor()
+
+# cursor.execute
+# columns - 
+# for col
+
+conn.close()
---
+++ queries/identify_Headers.sql
@@ -0,0 +1,1 @@
+SELECT * FROM crimes LIMIT 1; 
