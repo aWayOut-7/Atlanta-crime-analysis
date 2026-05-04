ea9ffb0de2bda9df0f82bd48beabeb4e290b1bf4
Wae Outte.J <j.mckenz@live.com>
2026-05-04 15:23:38 -0400

    Changes to be committed: 	modified:   app/run_query.py

--- app/run_query.py
+++ app/run_query.py
@@ -7,9 +7,10 @@ import sys
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 database_path = os.path.join(base_dir, 'atlanta_crime.db')
 
+#Sql file path is passedd as terminal argument when running 
 sql_file = sys.argv[1]
 
-#f shorthand for File 
+#Reads SQL file, f shorthand for File 
 with open(sql_file, 'r') as f: 
     query = f.read()
 
@@ -22,5 +23,8 @@ rows = cursor.fetchall()
 for row in rows: 
     print(row)
 
+#Print total count of rows returned by query
 print(f"\nTotal Results: {len(rows)}")
+
+#close connection to database
 conn.close()
