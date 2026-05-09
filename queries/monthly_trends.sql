5ab1d51d19df1ac45b5623127f22fb0298d5c277
Wae Outte.J <j.mckenz@live.com>
2026-05-09 18:30:49 -0400

    Removed Pruned Lines in Queries

--- queries/monthly_trends.sql
+++ queries/monthly_trends.sql
@@ -2,15 +2,6 @@
 -- This query counts the # of crime incidents for each month, excluding empty month values, and in descending order.
 --strftime(format, date) is used to extract the month & year from the occuredFromDate column
 
--- ========== pruned lines below ==========
--- select strftime('%Y-%m', OccurredFromDate) as Monthly_Count, COUNT(*) AS monthly_crime_count
--- from crimes where OccurredFromDate != '' 
--- group by strftime('%Y-%m', OccurredFromDate) 
--- order by Monthly_Count desc;
-
--- SELECT OccurredFromDate FROM crimes WHERE OccurredFromDate != '' LIMIT 5;
--- ========== pruned lines above ==========
-
 SELECT     
     substr(OccurredFromDate, instr(OccurredFromDate, ' ') - 4, 4 ) as YEAR,
     substr(OccurredFromDate, 1, instr(OccurredFromDate, '/') - 1) as MONTH,
